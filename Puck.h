f59e12c37dfc870c8e4600767eeb97b27ed8d5ef
elizpotapova <66255970+elizpotapova@users.noreply.github.com>
2020-06-22 20:54:11 +0300

    puck

---
+++ Puck.h
@@ -0,0 +1,34 @@
+#pragma once
+#include <SFML/Graphics.hpp>
+using namespace sf;
+using namespace std;
+
+class Puck
+{
+public:
+	int dx_Puck = 0;
+	int dy_Puck = 0;
+
+	Sprite Puck1;
+
+	Puck();
+	~Puck();
+
+	Sprite get() {
+		return Puck1;
+	}
+
+	void Ball_up() {
+		dx_Puck = 3;
+		dy_Puck = 2;
+		Puck1.move(dx_Puck, 0);
+		Puck1.move(0, dy_Puck);
+	}
+
+	void move_x() {
+		Puck1.move(dx_Puck, 0);
+	}
+
+	void move_y() {
+		Puck1.move(0, dy_Puck);
+	}
