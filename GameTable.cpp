e035b6359b725ae9037d2635c494acdc728b9bc8
Alex <sascha091001@mail.ru>
2020-06-22 15:44:32 +0300

    Game Fizika

--- GameTable.cpp
+++ GameTable.cpp
@@ -7,18 +7,21 @@
 using namespace sf;
 using namespace std;
 
+int _HP = 0;
+
 GameTable::GameTable()
 {
 	RenderWindow app(VideoMode(800, 600), "Alex!");
 	menu(app);
 }
 
+
 void menu(RenderWindow & window) {
 	Texture menuTexture1, menuTexture2, menuTexture3, aboutTexture, menuBackground;
 	menuTexture1.loadFromFile("C:/Users/Alex/source/repos/Arcanoid_Project/Arcanoid_Project/images/start.png");
 	menuTexture2.loadFromFile("C:/Users/Alex/source/repos/Arcanoid_Project/Arcanoid_Project/images/about.png");
 	menuTexture3.loadFromFile("C:/Users/Alex/source/repos/Arcanoid_Project/Arcanoid_Project/images/exit.png");
-	aboutTexture.loadFromFile("C:/Users/Alex/source/repos/Arcanoid_Project/Arcanoid_Project/images/i.jpg");
+	aboutTexture.loadFromFile("C:/Users/Alex/source/repos/Arcanoid_Project/Arcanoid_Project/images/aboutBackground.jpg");
 	menuBackground.loadFromFile("C:/Users/Alex/source/repos/Arcanoid_Project/Arcanoid_Project/images/menu.png");
 	Sprite menu1(menuTexture1), menu2(menuTexture2), menu3(menuTexture3), about(aboutTexture), menuBg(menuBackground);
 	bool isMenu = 1;
@@ -44,7 +47,7 @@ void menu(RenderWindow & window) {
 			if (menuNum == 1) {
 				isMenu = false;
 				window.close();
-				getHP();
+				//getHP();
 			}
 
 			if (menuNum == 2) { window.draw(about); window.display(); while (!Keyboard::isKeyPressed(Keyboard::Escape)); }
@@ -58,3 +61,128 @@ void menu(RenderWindow & window) {
 		window.display();
 	}
 }
+
+
+bool isCollide(Sprite s1, Sprite s2) {
+	return s1.getGlobalBounds().intersects(s2.getGlobalBounds());
+}
+
+
+void GameRun() {
+	_HP--;
+	int Score = 0;
+	PuckSupply PS(_HP);
+	BrickPile BP; //Стена
+	Paddle puddle1;//Телега
+	Puck Puck1;   //Шайба1
+	RenderWindow app(VideoMode(800, 600), "Gorshkonoid!");
+	app.setFramerateLimit(60);
+	Texture	BG;
+	BG.loadFromFile("C:/Users/Alex/source/repos/Arcanoid_Project/Arcanoid_Project/images/background.jpg");
+
+	Sprite background(BG);
+
+	float dx_Paddle = 6;
+	bool pusk = true;
+	//float dx_Puck = 6;
+	//float dy_Puck = 0;
+
+	int n1 = BP.n;
+
+	while (app.isOpen()) 
+	{
+		Event e;
+		while (app.pollEvent(e)) {
+			if (e.type == Event::Closed) {
+				app.close();
+			}
+		}
+		
+
+			Puck1.move_x();
+			for (int i = 0; i <= BP.n; i++) {
+				if (isCollide(BP.block[i], Puck1.get())) {
+					BP.block[i].setPosition(-100, 0);
+					Puck1.dx_Puck = -Puck1.dx_Puck;
+					Score = Score + 100;
+					cout << "Score: " << Score << endl;
+					--n1;
+
+				}
+			}
+
+			Puck1.move_y();
+			for (int i = 0; i <= BP.n; i++) {
+				if (isCollide(BP.block[i], Puck1.get())) {
+					BP.block[i].setPosition(-100, 0);
+					Puck1.dy_Puck = -Puck1.dy_Puck;
+					Score = Score + 100;
+					cout << "Score: " << Score << endl;
+					--n1;
+				}
+			}
+
+			Vector2f a = Puck1.get().getPosition();
+
+			
+
+			Vector2f b = puddle1.getPaddle().getPosition();
+
+			if (n1 == 0) { app.close(); }//GameWin(); }
+			if (a.y > 560)
+			{
+				if (_HP < 1)
+				{
+					app.close();
+					//GameLose();
+				}
+				
+				_HP--;
+				Puck1.dx_Puck = 0;
+				Puck1.dy_Puck = 0;
+				Puck1.Puck1.setPosition(330, 540);
+				puddle1.Paddle1.setPosition(300, 580);
+				pusk = true;
+			}
+
+			if (Keyboard::isKeyPressed(Keyboard::Right)) puddle1.Right();
+			if (Keyboard::isKeyPressed(Keyboard::Left)) puddle1.Left();
+			if (Keyboard::isKeyPressed(Keyboard::Space)) Sleep(2000);
+
+
+			if ((pusk == true) and (Keyboard::isKeyPressed(Keyboard::Up))) //Правим
+			{
+				Puck1.Ball_up();
+				pusk = false;
+			}
+
+			
+
+			if (b.x > 705) {    //Попроавили
+				puddle1.break_L();
+			}
+
+			if (b.x < 0) {    //Поправили
+				puddle1.break_R();
+			}
+
+			if (isCollide(puddle1.getPaddle(), Puck1.get())) { //Поправили
+				Puck1.dy_Puck = -Puck1.dy_Puck;
+				//dy_Puck = -dy_Puck;
+			}
+
+			app.clear();
+			app.draw(background);
+			app.draw(Puck1.get());
+			app.draw(puddle1.getPaddle());
+
+			for (int i = 0; i < BP.n; i++) {
+				app.draw(BP.block[i]);
+			}
+
+			app.display();
+	}
+}
+
+
+
