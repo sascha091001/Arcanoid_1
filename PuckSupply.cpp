0fbac31d1290a555834e560e25afe75a728c8106
elizpotapova <66255970+elizpotapova@users.noreply.github.com>
2020-06-22 19:40:45 +0300

    puckSupply1

---
+++ PuckSupply.cpp
@@ -0,0 +1,13 @@
+#include "pch.h"
+#include "PuckSupply.h"
+#include <iostream>
+using namespace std;
+
+
+PuckSupply::PuckSupply(int HP)
+{
+	_Puck_arr = new Puck[HP];
+	_cnt = HP;
+}
+
+
---
+++ PuckSupply.h
@@ -0,0 +1,8 @@
+#pragma once
+#include <vector>
+#include "Puck.h"
+class PuckSupply
+{
+
+};
+
