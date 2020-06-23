885d88ba2b4ceda0fd326dbb45181c1cb32464b1
Alex <sascha091001@mail.ru>
2020-06-23 12:22:25 +0300

    With Bricks

---
+++ Brick.cpp
@@ -0,0 +1,14 @@
+#include "pch.h"
+#include "Brick.h"
+
+
+
+Brick::Brick()
+{
+	Brick_Pict.loadFromFile("C:/Users/Alex/source/repos/Arcanoid_Project/Arcanoid_Project/images/brick_2_3.png");
+}
+
+
+Brick::~Brick()
+{
+}
