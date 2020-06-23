8e543b54ecbbf87e6e2a83a2e0158b11bec3353e
yurin <yurin.daniil@inbox.ru>
2020-06-23 11:59:20 +0300

    1 Itap

---
+++ GameTable.h
@@ -0,0 +1,22 @@
+#pragma once
+#include <iostream>
+#include <SFML/Graphics.hpp>
+#include "BrickPile.h"
+#include "PuckSupply.h"
+#include "Paddle.h"
+#include "Puck.h"
+
+using namespace sf;
+
+class GameTable
+{
+public:
+	GameTable();
+	~GameTable();
+};
+
+void menu(RenderWindow & window);
+void GameRun();
+void GameWin();
+void GameLose();
+void getHP();
